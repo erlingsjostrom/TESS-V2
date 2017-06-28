b1032521fd895e6292fbd06572b73073575b1dbd
lundmeri <lundmeri@WL350844>
2017-06-28 13:22:20 +0200

    Add project files.

---
+++ TestRestfulAPI/Models/User.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestRestfulAPI.Models
+{
+    public class User
+    {
+        public int Id { get; }
+
+        public string Name { get; }
+
+        public User(int id)
+        {
+            this.Id = id;
+            if (id == 1)
+            {
+                this.Name = "Anton";
+            }
+            else if (id == 2)
+            {
+                this.Name = "Erik";
+            }
+        }
+
+        public static ICollection<User> all()
+        {
+            return new List<User>()
+            {
+                new User(1),
+                new User(2)
+            };
+        }
+    }
+}
