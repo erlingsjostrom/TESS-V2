7f0216e1bd09c99ad05330b1d2314e8c243d484a
Erik Lundmark <cjiqqq@gmail.com>
2017-07-06 13:22:17 +0200

    Update to code first

---
+++ TestRestfulAPI.Entities.User/Permission.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestRestfulAPI.Entities.User
+{
+    public class Permission : NamedEntity
+    {
+        public Permission()
+        {
+            this.Roles = new HashSet<Role>();
+        }
+
+        // Properties
+
+        // Relations
+        public virtual ICollection<Role> Roles { get; set; }
+    }
+}
---
+++ TestRestfulAPI.Entities.User/Resource.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestRestfulAPI.Entities.User
+{
+    public class Resource : NamedEntity
+    {
+        public Resource()
+        {
+            this.Users = new HashSet<User>();
+        }
+        // Properties
+        public string Location { get; set; }
+
+        // Relations
+        public virtual ICollection<User> Users { get; set; }
+    }
+}
