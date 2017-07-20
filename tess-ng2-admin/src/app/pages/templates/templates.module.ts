8ee13a3dadb7fba1d36174eedcff67d7073b4b8b
Halvarsson Jonas <jonas.halvarsson@tieto.com>
2017-07-20 11:34:28 +0200

    fixed templates menu bug

--- tess-ng2-admin/src/app/pages/templates/templates.module.ts
+++ tess-ng2-admin/src/app/pages/templates/templates.module.ts
@@ -2,7 +2,7 @@ import { NgModule }      from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { NgaModule } from '../../theme/nga.module';
-import { Offers } from './templates.component';
+import { Templates } from './templates.component';
 import { routing } from './templates.routing';
 
 
